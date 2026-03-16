4fd014e815d33f30ab2181fb35ed197dc110278d
T0mike <tomarares334@gmail.com>
2026-03-16 21:06:11 +0200

    add initial classes and update readme

--- main.cpp
+++ main.cpp
@@ -1,55 +1,120 @@
 #include <iostream>
-#include <array>
-#include "include/Example.h"
-// This also works if you do not want `include/`, but some editors might not like it
-// #include "Example.h"
+#include <vector>
+
+class Weapon {
+    std::string name;
+    int damage;
+    double fireRate;
+    int ammo;
+    int range;
+public:
+    Weapon(const std::string& name, int damage, double fireRate, int ammo, int range){
+        this->name = name;
+        this->damage = damage;
+        this->fireRate = fireRate;
+        this->ammo = ammo;
+        this->range = range;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Weapon& w) {
+        os << "name: " << w.name << "\n";
+        os << "damage: " << w.damage << "\n";
+        os << "fireRate: " << w.fireRate << "\n";
+        os << "ammo: " << w.ammo << "\n";
+        os << "range: " << w.range << "\n";
+        return os;
+    }
+};
+
+class Player{
+    std::string name;
+    int hp;
+    double speed;
+    double x, y;
+    std::vector<Weapon> weapons;
+
+public:
+    Player(const std::string& name, int hp, double speed, double x, double y){
+        this->name = name;
+        this->hp = hp;
+        this->speed = speed;
+        this->x = x;
+        this->y = y;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Player& p) {
+        os << "name: " << p.name << "\n";
+        os << "hp: " << p.hp << "\n";
+        os << "speed: " << p.speed << "\n";
+        os << "x: " << p.x << " " << "y: " << p.y << "\n";
+        for(const auto& w : p.weapons)
+            os << w;
+        return os;
+    }
+
+    void addWeapon(const Weapon& w){
+        weapons.push_back(w);
+    }
+};
+
+class Zombie{
+    std::string name;
+    int hp;
+    double speed;
+    double x, y;
+    int damage;
+
+public:
+    Zombie(const std::string& name, int hp, double speed, double x, double y, int damage){
+        this->name = name;
+        this->hp = hp;
+        this->speed = speed;
+        this->x = x;
+        this->y = y;
+        this->damage = damage;
+    }
+
+    Zombie(const Zombie& z) {
+        this->name = z.name;
+        this->hp = z.hp;
+        this->speed = z.speed;
+        this->x = z.x;
+        this->y = z.y;
+        this->damage = z.damage;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Zombie& z) {
+        os << "name: " << z.name << "\n";
+        os << "hp: " << z.hp << "\n";
+        os << "speed: " << z.speed << "\n";
+        os << "x: " << z.x << " " << "y: " << z.y << "\n";
+        os << "damage: " << z.damage << "\n";
+        return os;
+    }
+
+    Zombie& operator=(const Zombie& other) {
+        this->name = other.name;
+        this->hp = other.hp;
+        this->speed = other.speed;
+        this->x = other.x;
+        this->y = other.y;
+        this->damage = other.damage;
+        return *this;
+    }
+
+    ~Zombie() {
+        std::cout << "Zombie " << name << " destroyed\n";
+    }
+};
 
 int main() {
-    std::cout << "Hello, world!\n";
-    Example e1;
-    e1.g();
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
-    }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
+    Weapon pistol("pistol", 10, 1, 10, 20);
+    std::cout<<pistol;
+    Player Tomike("Tomike", 200, 20, 2.5, 3.5);
+    Tomike.addWeapon(pistol);
+    Zombie Mihai("Mihai", 50, 5, 2.5, 3.5, 5);
+    std::cout<<pistol;
+    std::cout<<Tomike;
+    std::cout<<Mihai;
     return 0;
 }
